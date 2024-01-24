9059eb056a13d76f20d7c96112d94d8b6daa31d8
itsshawondevs <shawonuser24@gmail.com>
2024-01-24 13:37:02 +0600

    calculator design

---
+++ script.js
@@ -0,0 +1,29 @@
+let topResult = document.querySelector(".top_result")
+let result = document.querySelector(".result")
+
+let ac = document.querySelector(".ac")
+let ce = document.querySelector(".ce")
+let ans = document.querySelector(".ans")
+let parcent = document.querySelector(".parcent")
+
+let one = document.querySelector(".one")
+let two = document.querySelector(".two")
+let three = document.querySelector(".three")
+let four = document.querySelector(".four")
+let five = document.querySelector(".five")
+let six = document.querySelector(".six")
+let seven = document.querySelector(".seven")
+let eight = document.querySelector(".eight")
+let nine = document.querySelector(".nine")
+let jiro = document.querySelector(".jiro")
+let point = document.querySelector(".point")
+
+let into = document.querySelector(".into")
+let divided = document.querySelector(".divided")
+let plus = document.querySelector(".plus")
+let minus = document.querySelector(".minus")
+let equal = document.querySelector(".equal")
+
+one.addEventListener("click",function(){
+    result.innerHTML += 1
+})
