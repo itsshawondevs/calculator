d8321234200c0cc60ffc7d9582524853aa5b0e86
itsshawondevs <shawonuser24@gmail.com>
2024-01-25 13:09:43 +0600

    responsive upgrade

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        screens: {
+            'xs': '400px',
+            'sm': '576px',
+            'md': '768px',
+            'lg': '992px',
+            'xl': '1280px',
+            '2xl': '1436px',
+        }, 
+      }
+    }
+  }
